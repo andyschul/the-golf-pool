be5be3a18bf125ee8dc107b4e6df51bfb87ec3da
Andrew Schultz <abschultz1@gmail.com>
2019-02-13 19:26:23 -0500

    Await for api calls to finish before attempting to create schedulers

--- api.js
+++ api.js
@@ -1,8 +1,13 @@
+const axios = require('axios');
+const redis = require('redis')
+
+const client = redis.createClient(process.env.REDIS_URL);
 const CONTESTS = ["The Open Championship", "U.S. Open", "Masters Tournament", "PGA Championship", "THE PLAYERS Championship"];
+
 
 function getYearlySchedule() {
   let currentYear = (new Date()).getFullYear();
-  axios.get(`${process.env.SPORTS_RADAR_URI}/schedule/pga/${currentYear}/tournaments/schedule.json?api_key=${process.env.SPORTS_RADAR_API_KEY}`)
+  return axios.get(`${process.env.SPORTS_RADAR_URI}/schedule/pga/${currentYear}/tournaments/schedule.json?api_key=${process.env.SPORTS_RADAR_API_KEY}`)
     .then(function (response) {
       let tournaments = response.data.tournaments.filter(x=>CONTESTS.includes(x.name)).map(x=>(
         {
@@ -15,14 +20,15 @@ function getYearlySchedule() {
       return {tournaments: tournaments};
     })
     .catch(function (error) {
-      console.log(error);
+      console.log('error getYearlySchedule', error);
+      return {};
     });
-},
+}
 
 
 function getTeeTimes() {
-  let currentYear (new Date()).getFullYear();
-  axios.get(`${process.env.SPORTS_RADAR_URI}/teetimes/pga/${currentYear}/tournaments/${tourneyId}/rounds/1/teetimes.json?api_key=${process.env.SPORTS_RADAR_API_KEY}`)
+  let currentYear = (new Date()).getFullYear();
+  return axios.get(`${process.env.SPORTS_RADAR_URI}/teetimes/pga/${currentYear}/tournaments/${tourneyId}/rounds/1/teetimes.json?api_key=${process.env.SPORTS_RADAR_API_KEY}`)
     .then(function (response) {
       // TODO: create a list of lists for pairings
       let pairings = response.data.round.courses[0].pairings
@@ -31,7 +37,8 @@ function getTeeTimes() {
       return {pairings: pairings};
     })
     .catch(function (error) {
-      console.log(error);
+      console.log('error getTeeTimes', error);
+      return {};
     });
 }
 
--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const redis = require('redis')
 const schedule = require('node-schedule');
 const {promisify} = require('util');
@@ -7,34 +6,44 @@ require('dotenv').config();
 
 const client = redis.createClient(process.env.REDIS_URL);
 const getAsync = promisify(client.get).bind(client);
-
+let currentYear = (new Date()).getFullYear();
 
 // GET TOURNAMENT SCHEDULE ONCE PER YEAR
 schedule.scheduleJob('0 0 0 0 *', function(){
   api.getYearlySchedule();
 });
-
+console.log(`Created scheduler: Yearly Tournament Schedule`)
 
 // CREATE INDIVIDUAL TOURNAMENT SCHEDULERS
 getAsync(`schedule:${currentYear}`).then(function(res) {
-  if (res) {
-    let tourneySchedule = JSON.parse(res);
-  } else {
-    let tourneySchedule = api.getYearlySchedule();
-  }
+  async function createSchedulers() {
+    let tourneySchedule;
+    if (res) {
+      tourneySchedule = JSON.parse(res);
+      console.log('redis')
+    } else {
+      tourneySchedule = await api.getYearlySchedule();
+    }
+    if (!tourneySchedule.hasOwnProperty('tournaments')) {
+      console.error('Could not get tournament schedule')
+      return;
+    }
 
-  let now = new Date();
-  for (let tourney of tourneySchedule.tournaments) {
-    if (new Date(tourney.start_date) > now) {
-      // TODO: create cron from tourney.start_date
-      let cron;
+    let now = new Date();
+    for (let tourney of tourneySchedule.tournaments) {
+      if (new Date(tourney.start_date) > now) {
+        // TODO: create cron from tourney.start_date
+        let cron = '0 0 0 0 *';
 
-      schedule.scheduleJob(cron, function(){
-        let teeTimes = api.getTeeTimes(tourney.id);
-        if (teeTimes.hasOwnPropery('pairings')) {
-          this.cancel();
-        }
-      });
+        schedule.scheduleJob(cron, function(){
+          let teeTimes = api.getTeeTimes(tourney.id);
+          if (teeTimes.hasOwnPropery('pairings')) {
+            this.cancel();
+          }
+        });
+        console.log(`Created scheduler: ${tourney.name} ${currentYear}`)
+      }
     }
   }
+  createSchedulers();
 });
