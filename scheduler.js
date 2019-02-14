4ad54dc6ec337770adaa378a365c33fbaa45f1dc
Andrew Schultz <abschultz1@gmail.com>
2019-02-13 21:38:49 -0500

    Make createSchedulers async

--- scheduler.js
+++ scheduler.js
@@ -6,50 +6,46 @@ require('dotenv').config();
 
 const client = redis.createClient(process.env.REDIS_URL);
 const getAsync = promisify(client.get).bind(client);
-let currentYear = (new Date()).getFullYear();
 
 // TRIGGER TOURNAMENT SCHEDULER CREATION ONCE PER YEAR
 schedule.scheduleJob('0 0 1 1 *', function(){
   createSchedulers();
 });
-console.log(`Created scheduler: Yearly Tournament Scheduler`)
+console.log(`Created scheduler: Yearly Tournament Scheduler`);
 
 
 // CREATE INDIVIDUAL TOURNAMENT SCHEDULERS
-function createSchedulers() {
-  getAsync(`schedule:${currentYear}`).then(function(res) {
-    async function f() {
-      let tourneySchedule;
-      if (res) {
-        tourneySchedule = JSON.parse(res);
-      } else {
-        tourneySchedule = await api.getYearlySchedule();
-      }
-      if (!tourneySchedule.hasOwnProperty('tournaments')) {
-        console.error('Could not get tournament schedule')
-        return;
-      }
+async function createSchedulers() {
+  let currentYear = (new Date()).getFullYear();
+  let tourneySchedule;
+  let res = await getAsync(`schedule:${currentYear}`);
+  if (res) {
+    tourneySchedule = JSON.parse(res);
+  } else {
+    tourneySchedule = await api.getYearlySchedule();
+  }
+  if (!tourneySchedule.hasOwnProperty('tournaments')) {
+    console.error('Could not get tournament schedule');
+    return;
+  }
 
-      let now = new Date();
-      for (let tourney of tourneySchedule.tournaments) {
-        let tourneyDate = new Date(`${tourney.start_date}T00:00:00`);
-        if (tourneyDate > now) {
-          // Create scheduler to run every 5th hour, starting 3 days prior to tournament
-          let startDay = new Date(tourneyDate.getTime());
-          startDay.setDate(startDay.getDate() - 3);
+  let now = new Date();
+  for (let tourney of tourneySchedule.tournaments) {
+    let tourneyDate = new Date(`${tourney.start_date}T00:00:00`);
+    if (tourneyDate > now) {
+      // Create scheduler to run every 5th hour, starting 3 days prior to tournament
+      let startDay = new Date(tourneyDate.getTime());
+      startDay.setDate(startDay.getDate() - 3);
 
-          schedule.scheduleJob({ start: startDay, end: tourneyDate, rule: '0 */5 * * *' }, function(){
-            let teeTimes = api.getTeeTimes(tourney.id);
-            if (teeTimes.hasOwnPropery('pairings')) {
-              this.cancel();
-            }
-          });
-          console.log(`Created scheduler: ${tourney.name} ${currentYear}`)
+      schedule.scheduleJob({ start: startDay, end: tourneyDate, rule: '0 */5 * * *' }, function(){
+        let teeTimes = api.getTeeTimes(tourney.id);
+        if (teeTimes.hasOwnPropery('pairings')) {
+          this.cancel();
         }
-      }
+      });
+      console.log(`Created scheduler: ${tourney.name} ${currentYear}`);
     }
-    f();
-  });
+  }
 }
 
 createSchedulers();
