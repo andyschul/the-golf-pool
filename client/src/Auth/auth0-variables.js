9fc5ee6c476a2762cec96d1d396950c54885c896
Andrew Schultz <abschultz1@gmail.com>
2019-02-23 09:26:19 -0500

    change callback

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'started-from-a-golf-pool.auth0.com',
   clientId: 'kPWoEkujBsPG3fwRnir6KJaZ38HWOcrQ',
-  callbackUrl: 'https://started-from-a-golf-pool.herokuapp.com:3000/callback',
+  callbackUrl: 'https://started-from-a-golf-pool.herokuapp.com/callback',
   apiUrl: 'https://started-from-a-golf-pool.auth0.com/api/v2/'
 }
