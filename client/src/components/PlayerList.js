a272a7d9e53bc9870c70fbf6f3f6f74746a4a2e0
Andrew Schultz <abschultz1@gmail.com>
2019-03-10 15:34:55 -0400

    Group colors

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -8,6 +8,17 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import red from '@material-ui/core/colors/red';
+import pink from '@material-ui/core/colors/pink';
+import purple from '@material-ui/core/colors/purple';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+import yellow from '@material-ui/core/colors/yellow';
+import orange from '@material-ui/core/colors/orange';
+import brown from '@material-ui/core/colors/brown';
+import grey from '@material-ui/core/colors/grey';
 
 const styles = theme => ({
   root: {
@@ -15,6 +26,46 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
     marginTop: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2,
+  },
+  0: {
+    backgroundColor: red[500],
+    color: theme.palette.common.white,
+  },
+  1: {
+    backgroundColor: pink[500],
+    color: theme.palette.common.white,
+  },
+  2: {
+    backgroundColor: purple[500],
+    color: theme.palette.common.white,
+  },
+  3: {
+    backgroundColor: blue[500],
+    color: theme.palette.common.white,
+  },
+  4: {
+    backgroundColor: green[500],
+    color: theme.palette.common.white,
+  },
+  5: {
+    backgroundColor: yellow[500],
+    color: theme.palette.common.black,
+  },
+  6: {
+    backgroundColor: orange[500],
+    color: theme.palette.common.black,
+  },
+  7: {
+    backgroundColor: brown[500],
+    color: theme.palette.common.white,
+  },
+  8: {
+    backgroundColor: grey[50],
+    color: theme.palette.common.black,
+  },
+  9: {
+    backgroundColor: grey[900],
+    color: theme.palette.common.white,
   },
 });
 
@@ -49,8 +100,14 @@ class PlayerList extends Component {
       <div className={classes.root}>
         <Paper>
           <List component="nav">
-            <ListItem>
-              <ListItemText primary={`Group ${groupIndex+1} ${this.state.locked ? ' (LOCKED)' : ''}`} />
+            <ListItem className={this.props.classes[groupIndex]}>
+              <ListItemText primary={
+                <React.Fragment>
+                  <Typography className={this.props.classes[groupIndex]}>
+                    {`Group ${groupIndex+1} ${this.state.locked ? ' (LOCKED)' : ''}`}
+                  </Typography>
+                </React.Fragment>
+              } />
             </ListItem>
             {players.map(player => (
               <Player key={player.id} {...player} locked={this.state.locked} onClick={() => selectPlayer(player.id, groupIndex)} />
