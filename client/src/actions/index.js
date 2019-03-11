c39e3002cf968d3ddf171e36497e7f499925aedc
Andrew Schultz <abschultz1@gmail.com>
2019-03-11 09:06:12 -0400

    Load dummy data

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -111,6 +111,23 @@ export function scheduleFetchDataSuccess(schedule) {
     };
 }
 
+
+let data = [ { id: 'b79a87f9-e868-4e10-8c42-2f06d7630f2a',
+  name: 'THE PLAYERS Championship',
+  start_date: '2019-03-14' },
+{ id: 'b404a8d5-5e33-4417-ae20-5d4d147042ee',
+  name: 'Masters Tournament',
+  start_date: '2019-04-11' },
+{ id: 'b850e0a9-c15d-4263-8122-03df781e2b8d',
+  name: 'PGA Championship',
+  start_date: '2019-05-16' },
+{ id: 'd59637dc-7f7a-4cd8-a4af-0c5d419a9f39',
+  name: 'U.S. Open',
+  start_date: '2019-06-13' },
+{ id: '4524eac8-9713-43c5-a742-290d8ab434ba',
+  name: 'The Open Championship',
+  start_date: '2019-07-18' } ];
+
 export function scheduleFetchData(url) {
     return (dispatch) => {
         dispatch(scheduleIsLoading(true));
@@ -122,7 +139,7 @@ export function scheduleFetchData(url) {
                 dispatch(scheduleIsLoading(false));
                 return response;
             })
-            .then((response) => response.json())
+            .then((response) => data)
             .then((schedule) => dispatch(scheduleFetchDataSuccess(schedule)))
             .catch(() => dispatch(scheduleHasErrored(true)));
     };
