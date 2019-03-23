c26e46ac099c55642d3d89f4c3eef8b1ee05622d
Andrew Schultz <abschultz1@gmail.com>
2019-03-23 11:41:08 -0400

    Site images

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom';
 import store from './store/configureStore';
-import Routes from './Routes';
+import Routes from './routes';
 import history from './history';
 require('dotenv').config({path: '../.env', silent: process.env.NODE_ENV === 'production'});
 
